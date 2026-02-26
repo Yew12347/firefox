eac31edbd2fe0414ddcc6eb70a44478462f440d2
Emilio Cobos Álvarez <emilio@crisal.io>
2026-02-26 11:53:52 +0100

    Bug 2017512 - Sanity check adopted stylesheet setters / deleters. r=edgar,#style,#layout a=pascalc
    Differential Revision: https://phabricator.services.mozilla.com/D284097

--- dom/base/DocumentOrShadowRoot.cpp
+++ dom/base/DocumentOrShadowRoot.cpp
@@ -112,6 +112,10 @@ void DocumentOrShadowRoot::RemoveSheetFromStylesIfApplicable(
 void DocumentOrShadowRoot::OnSetAdoptedStyleSheets(StyleSheet& aSheet,
                                                    uint32_t aIndex,
                                                    ErrorResult& aRv) {
+  if (aIndex > mAdoptedStyleSheets.Length()) [[unlikely]] {
+    MOZ_ASSERT_UNREACHABLE("Out of sync proxy");
+    return;
+  }
   Document& doc = *AsNode().OwnerDoc();
   // 1. If value’s constructed flag is not set, or its constructor document is
   // not equal to this DocumentOrShadowRoot's node document, throw a
@@ -164,7 +168,10 @@ void DocumentOrShadowRoot::OnSetAdoptedStyleSheets(StyleSheet& aSheet,
 void DocumentOrShadowRoot::OnDeleteAdoptedStyleSheets(StyleSheet& aSheet,
                                                       uint32_t aIndex,
                                                       ErrorResult&) {
-  MOZ_ASSERT(mAdoptedStyleSheets.ElementAt(aIndex) == &aSheet);
+  if (mAdoptedStyleSheets.ElementAt(aIndex) != &aSheet) [[unlikely]] {
+    MOZ_ASSERT_UNREACHABLE("Out of sync proxy");
+    return;
+  }
   mAdoptedStyleSheets.RemoveElementAt(aIndex);
   auto existingIndex = mAdoptedStyleSheets.LastIndexOf(&aSheet);
   if (existingIndex != mAdoptedStyleSheets.NoIndex && existingIndex >= aIndex) {
